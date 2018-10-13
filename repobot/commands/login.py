0a72f5c1b4f4b62798f25193942d8934b35f7f9b
Nicholas Palenchar <nickpalenchar@users.noreply.github.com>
2018-10-13 14:54:45 -0400

    prompts for key

--- repobot/commands/login.py
+++ repobot/commands/login.py
@@ -3,11 +3,26 @@
 
 from .base import Base
 
+import keyring
+import sys
+import re
+
 class Login(Base):
     '''login class'''
 
     def run(self):
-        if self.options['<token>'] is None:
-            self.options['<token>'] = input("Provide a personal access token:\n> ")
+        token = self.options['<token>'] 
+        
+        if token is None:
+            token = input("Provide a personal access token:\n> ")
 
+        if keyring.get_password('repobot', 'auth') is not None:
+            # @TODO get the -f flag
+            print('Auth token already exists! (*****%s)' % keyring.get_password('repobot', 'auth')[-6:])
+            confirm = input('Replace ******%s with new key? [y/N]')
+            if re.match('ye?s?', confirm, flags=2) is None:
+                sys.exit(0)
+            #@TODO: do a keyring test
 
+        keyring.set_password('repobot', 'auth', token)
+        print('Successfully authenticated')        
